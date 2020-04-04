d10c5e888830ef8b89452253aef9a4579e1c61f3
miqbalboy19 <kokonutboyz@gmail.com>
2020-04-04 14:21:47 +0700

    Modul 4 PDKP

---
+++ C#/Modul4_Kel39/Modul4_Kel39/Percobaan2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul4_Kel39
+{
+    class Class1
+    {
+    }
+}
---
+++ C#/Modul4_Kel39/Modul4_Kel39/Percobaan2Class.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul4_Kel39
+{
+    class Percobaan2Class
+    {
+        public void pembagian(float a, float b)
+        {
+            double c = a / b;
+            Console.WriteLine("Hasil pembagian {0} dengan {1} adalah {2}", a, b, c);
+        }
+        public int pengurangan(int a, int b)
+        {
+            int c = a - b;
+            return c;
+        }
+
+    }
+}
